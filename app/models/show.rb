6ff8ffd1cc4027e897e6d567559c997c3b21c303
Elle Faraday <faraday.elle@gmail.com>
2018-01-14 19:31:14 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,10 +1,26 @@
 class Show < ActiveRecord::Base
 
   def self.highest_rating
-    Show.maximum(:rating)
+    self.maximum(:rating)
   end
 
   def self.most_popular_show
     self.find_by(rating: self.highest_rating)
+  end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    self.find_by(rating: self.lowest_rating)
+  end
+
+  def self.ratings_sum
+    self.sum(:ratings)
+  end
+
+  def self.popular_shows
+    self.where("rating > ?" 5)
   end
 end
